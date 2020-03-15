579b92a2f3c1ed63d828254394651c9ffb710dd0
David <david.c.chicharro@gmail.com>
2020-03-16 00:59:13 +0100

    Añadidos primeros iconos

--- app_tfg/resources/views/incidents/map.blade.php
+++ app_tfg/resources/views/incidents/map.blade.php
@@ -7,6 +7,7 @@
 @section('content')
     <main class="col-7 mt-4 p-5">
         <h2>Mapa de incidentes</h2>
+        <img class="img-fluid" src="{{asset('images/mapa-grx.png')}}">
         <p>Body content</p>
 
     </main>
