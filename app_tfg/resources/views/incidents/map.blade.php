3f6f41fade78c9c013ba2acaf4a921669f581ff9
David <david.c.chicharro@gmail.com>
2020-04-04 12:46:07 +0200

    Limpieza y reorganización de código

--- app_tfg/resources/views/incidents/map.blade.php
+++ app_tfg/resources/views/incidents/map.blade.php
@@ -5,16 +5,13 @@
 
 
 @section('content')
-	{{--    <main class="col-8 col-lg-7 mt-4 p-5">--}}
 	<h2>Mapa de incidentes</h2>
 	<section class="main-content mx-1">
 		<img class="img-fluid w-100" src="{{asset('images/mapa-grx.png')}}">
 		<div class="my-3">
-			<a href="#">Mis publicaciones de incidentes</a>
+			<a href="/mis-publicaciones-incidentes">Mis publicaciones de incidentes</a>
 			<a class="float-right" href="/lista-incidentes">Ver lista</a>
 		</div>
-		<p>Body content</p>
 	</section>
 
-	{{--    </main>--}}
 @endsection
